8d7b911e0dde6e3ab6f63e18d4e2a63203ba9ef4
kan-215 <katamialvin369@gmail.com>
2024-03-28 23:49:08 +0300

    pall

--- main.c
+++ main.c
@@ -10,22 +10,21 @@ int main(int argc, char *argv[]) {
     int value;
     int line_number = 0;
 
-    /* Check if a file argument is provided */
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    /* Open the Monty bytecode file */
     file = fopen(argv[1], "r");
     if (file == NULL) {
         fprintf(stderr, "Error: Unable to open file %s\n", argv[1]);
         return EXIT_FAILURE;
     }
 
-    /* Parse and execute the bytecode commands */
     while (fscanf(file, "%s", opcode) != EOF) {
         line_number++;
+
+        printf("%s\n", opcode); /* Print the Monty bytecode command */
 
         if (strcmp(opcode, "push") == 0) {
             if (fscanf(file, "%d", &value) != 1) {
@@ -43,10 +42,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    /* Close the file */
     fclose(file);
 
-    /* Free the memory allocated for the stack */
     while (stack != NULL) {
         Stack *temp = stack;
         stack = stack->next;
