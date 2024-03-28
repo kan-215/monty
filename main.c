0f97d4c08785b0bd69362a2b23283891035b73a6
kan-215 <katamialvin369@gmail.com>
2024-03-29 00:28:23 +0300

    pall

--- main.c
+++ main.c
@@ -3,6 +3,27 @@
 #include <string.h>
 #include "monty.h"
 
+/**
+ * cleanup_stack - Frees memory allocated for the stack
+ * @stack: Pointer to the top of the stack
+ */
+void cleanup_stack(Stack *stack)
+{
+	while (stack != NULL)
+	{
+		Stack *temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
+}
+
+/**
+ * main - Entry point of the Monty interpreter
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
 int main(int argc, char *argv[])
 {
 	FILE *file;
@@ -13,51 +34,47 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-	fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-	return (EXIT_FAILURE);
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return (EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-	fprintf(stderr, "Error: Unable to open file %s\n", argv[1]);
-	return (EXIT_FAILURE);
+		fprintf(stderr, "Error: Unable to open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
 	}
 
 	while (fscanf(file, "%s", opcode) != EOF)
 	{
-	line_number++;
+		line_number++;
 
-	if (strcmp(opcode, "push") == 0)
-	{
-	if (fscanf(file, "%d", &value) != 1)
-	{
-	fprintf(stderr, "L%d: usage: push integer\n", line_number);
-	fclose(file);
-	return (EXIT_FAILURE);
-	}
-	push(&stack, value);
-	}
-	else if (strcmp(opcode, "pall") == 0)
-	{
-	pall(stack);
-	}
-	else
-	{
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-	fclose(file);
-	return (EXIT_FAILURE);
-	}
+		if (strcmp(opcode, "push") == 0)
+		{
+			if (fscanf(file, "%d", &value) != 1)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				fclose(file);
+				cleanup_stack(stack);
+				return (EXIT_FAILURE);
+			}
+			push(&stack, value);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(stack);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			fclose(file);
+			cleanup_stack(stack);
+			return (EXIT_FAILURE);
+		}
 	}
 
 	fclose(file);
-
-	while (stack != NULL)
-	{
-	Stack *temp = stack;
-	stack = stack->next;
-	free(temp);
-	}
+	cleanup_stack(stack);
 
 	return (EXIT_SUCCESS);
 }
--- monty.c
+++ monty.c
@@ -1,22 +1,26 @@
 #include "monty.h"
 
+
 /* Function to push an element to the stack */
-void push(Stack **stack, int value) {
-    Stack *new_node = malloc(sizeof(Stack));
-    if (new_node == NULL) {
-        fprintf(stderr, "Error: Unable to allocate memory\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->data = value;
-    new_node->next = *stack;
-    *stack = new_node;
+void push(Stack **stack, int value)
+{
+	Stack *new_node = malloc(sizeof(Stack));
+	if (new_node == NULL) {
+	fprintf(stderr, "Error: Unable to allocate memory\n");
+	exit(EXIT_FAILURE);
+	}
+	new_node->data = value;
+	new_node->next = *stack;
+	*stack = new_node;
 }
 
 /* Function to print all the values on the stack */
-void pall(Stack *stack) {
-    Stack *current = stack;
-    while (current != NULL) {
-        printf("%d\n", current->data);
-        current = current->next;
-    }
+void pall(Stack *stack)
+{
+	Stack *current = stack;
+	while (current != NULL)
+	{
+	printf("%d\n", current->data);
+	current = current->next;
+	}
 }
