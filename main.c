ea7897b11905e5a041335add9cb3b0415e670d24
kan-215 <katamialvin369@gmail.com>
2024-03-31 14:01:44 +0300

    pall

--- main.c
+++ main.c
@@ -1,72 +1,74 @@
 #include "monty.h"
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1024
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    FILE *file;
-    stack_t *stack = NULL;
-    char line[MAX_LINE_LENGTH];
-    unsigned int line_number = 0;
-    char *opcode, *arg;
-    stack_t *temp;
+  FILE * file;
+  stack_t * stack = NULL;
+  char line[MAX_LINE_LENGTH];
+  unsigned int line_number = 0;
+  char * opcode, * arg;
+  stack_t * temp;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+	if (argc != 2)
+	{
+	fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+	return (EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+	return (EXIT_FAILURE);
+	}
+	while (fgets(line, sizeof(line), file) != NULL) 
+	{
+	line_number++;
 
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        line_number++;
+	if (strlen(line) > 0 && line[strlen(line) - 1] == '\n')
+	{
+	line[strlen(line) - 1] = '\0';
+	}
 
-        if (strlen(line) > 0 && line[strlen(line) - 1] == '\n')
-        {
-            line[strlen(line) - 1] = '\0';
-        }
+	opcode = strtok(line, " ");
+	arg = strtok(NULL, " ");
 
-        opcode = strtok(line, " ");
-        arg = strtok(NULL, " ");
+	if (opcode != NULL)
+	{
+	if (strcmp(opcode, "push") == 0 && arg != NULL)
+	{
+	int value = atoi(arg);
 
-        if (opcode != NULL)
-        {
-            if (strcmp(opcode, "push") == 0 && arg != NULL)
-            {
-                int value = atoi(arg);
-                push(&stack, line_number, value);
-            }
-            else if (strcmp(opcode, "pall") == 0)
-            {
-                pall(&stack, line_number);
-            }
-            else
-            {
-                fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-                fclose(file);
-                return EXIT_FAILURE;
-            }
-        }
-    }
+        push( & stack, line_number, value);
+	} else if 
+	(strcmp(opcode, "pall") == 0)
+      {
+	pall( & stack, line_number);
+	} else
+	{
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	fclose(file);
+	return (EXIT_FAILURE);
+	}
+	}
+	}
 
-    fclose(file);
+	fclose(file);
 
-    while (stack != NULL)
-    {
-        temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+	while (stack != NULL)
+	{
+	temp = stack;
+	stack = stack -> next;
+	free(temp);
+	}
 
-    return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
--- monty.c
+++ monty.c
@@ -1,56 +1,56 @@
 #include "monty.h"
 
 /**
- * push - Pushes an element onto the stack.
- * @stack: Double pointer to the head of the stack.
+ * push -fuvtion tht Pushes an element onto the stack.
+ * @stack: Double pointer to the head node of the stack.
  * @line_number: The line number being executed from the script file.
- * @value: The value to be pushed onto the stack.
+ * @value: The value to be pushed to the stack.
  */
 void push(stack_t **stack, unsigned int line_number, int value)
 {
-    stack_t *new_node;
+	stack_t *new_node;
 
-    (void)line_number; /* Unused parameter */
+	(void)line_number;
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+	fprintf(stderr, "Error: malloc failed\n");
+	exit(EXIT_FAILURE);
+	}
 
-    new_node->n = value;
-    new_node->prev = NULL;
+	new_node->n = value;
+	new_node->prev = NULL;
 
-    if (*stack != NULL)
-    {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
-    }
-    else
-    {
-        new_node->next = NULL;
-    }
+	if (*stack != NULL)
+	{
+	new_node->next = *stack;
+	(*stack)->prev = new_node;
+	}
+	else
+	{
+	new_node->next = NULL;
+	}
 
-    *stack = new_node;
+	*stack = new_node;
 }
 
 /**
- * pall - Prints all the values on the stack, starting from the top.
- * @stack: Double pointer to the head of the stack.
+ * pall -function Prints all the values on the stack, starting from the top.
+ * @stack: Double pointer to the head node of the stack.
  * @line_number: The line number being executed from the script file.
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current;
+	stack_t *current;
 
-    (void)line_number; /* Unused parameter */
+	(void)line_number;
 
-    current = *stack;
+	current = *stack;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+	printf("%d\n", current->n);
+	current = current->next;
+	}
 }
