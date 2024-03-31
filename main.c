5eed16f59a7a45c0d74eca8f42d56b7994dc630f
kan-215 <katamialvin369@gmail.com>
2024-03-31 14:37:42 +0300

    pint

--- main.c
+++ main.c
@@ -1,74 +1,85 @@
 #include "monty.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1024
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  FILE * file;
-  stack_t * stack = NULL;
-  char line[MAX_LINE_LENGTH];
-  unsigned int line_number = 0;
-  char * opcode, * arg;
-  stack_t * temp;
+    FILE *file;
+    stack_t *stack = NULL;
+    char line[MAX_LINE_LENGTH];
+    unsigned int line_number = 0;
+    char *opcode, *arg;
+    stack_t *temp;
+    size_t len; /* Declare len here */
 
-	if (argc != 2)
-	{
-	fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-	return (EXIT_FAILURE);
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-	return (EXIT_FAILURE);
-	}
-	while (fgets(line, sizeof(line), file) != NULL) 
-	{
-	line_number++;
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-	if (strlen(line) > 0 && line[strlen(line) - 1] == '\n')
-	{
-	line[strlen(line) - 1] = '\0';
-	}
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        line_number++;
 
-	opcode = strtok(line, " ");
-	arg = strtok(NULL, " ");
+        /* Strip the end-of-line character */
+        len = strlen(line); /* Move declaration to the beginning of the block */
+        if (len > 0 && line[len - 1] == '\n')
+        {
+            line[len - 1] = '\0';
+        }
+      
+        opcode = strtok(line, " ");
+        arg = strtok(NULL, " ");
 
-	if (opcode != NULL)
-	{
-	if (strcmp(opcode, "push") == 0 && arg != NULL)
-	{
-	int value = atoi(arg);
+        if (opcode != NULL)
+        {
+            /* Print the opcode and argument (if any)*/
+            printf("%s", opcode);
+            if (arg != NULL)
+            {
+                printf(" %s\n", arg);
+            }
 
-        push( & stack, line_number, value);
-	} else if 
-	(strcmp(opcode, "pall") == 0)
-      {
-	pall( & stack, line_number);
-	} else
-	{
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-	fclose(file);
-	return (EXIT_FAILURE);
-	}
-	}
-	}
+            /* Execute the opcode*/
+            if (strcmp(opcode, "push") == 0 && arg != NULL)
+            {
+                int value = atoi(arg);
+                push(&stack, line_number, value);
+            }
+            else if (strcmp(opcode, "pint") == 0)
+            {
+                pint(&stack, line_number);
+            }
+            /* Add support for other opcodes as needed*/
+            else
+            {
+                fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+                fclose(file);
+                return EXIT_FAILURE;
+            }
+        }
+    }
 
-	fclose(file);
+    fclose(file);
 
-	while (stack != NULL)
-	{
-	temp = stack;
-	stack = stack -> next;
-	free(temp);
-	}
+    /* Free memory and exit*/
+    while (stack != NULL)
+    {
+        temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
 
-	return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
--- monty.h
+++ monty.h
@@ -37,5 +37,7 @@ typedef struct instruction_s
 /* Function prototypes */
 void push(stack_t **stack, unsigned int line_number, int value);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+
 
 #endif /* MONTY_H */
